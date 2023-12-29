7effcc717f6cd91b3e114c41db4473f8459f75ed
Vivek Tp <Vivektvp1@gmail.com>
2023-12-29 18:48:11 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -11,6 +11,9 @@ if (process.env.LOCAL_SERVER) {
 
 const PORT = process.env.PORT || 5001;
 
+const express = require('express');
+const app = express();
+
 app.listen(PORT, () => {
     console.log('Our app is running on port ${PORT}');
 });
